961a80df0344fc1749584081759c580b1d17464b
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-04-18 23:48:17 +0700

    Add Head tags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,19 +2,20 @@
   import { darkTheme } from '$lib/store';
   import Kofi from '$lib/components/Kofi.svelte';
   import SocialButtons from '$lib/components/SocialButtons.svelte';
+  import Head from '$lib/components/Head.svelte';
   import '../tailwind.css';
   import '../index.css';
   import { goto } from '$app/navigation';
 </script>
 
 <div class={$darkTheme ? 'dark' : ''}>
+  <Head />
   <button class="absolute left-2 top-2 text-lg" on:click={() => ($darkTheme = !$darkTheme)}>
     {$darkTheme ? '🌙' : '🌞'}
   </button>
   <button class="absolute right-2 top-2 text-lg dark:text-white text-black" on:click={() => goto("https://github.com/tinarskii/soundnep#contributing")}>
     Add sound
   </button>
-
   <slot />
   <Kofi name={'tinarskii'} />
   <SocialButtons />
