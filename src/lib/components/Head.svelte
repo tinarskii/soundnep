a1d9d62d3fbcec26c34658dc12f5d80c51c6e9ab
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-04-18 23:36:09 +0700

    Add image banner

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -1,6 +1,6 @@
 <script>
-  let title = "Sveltaily"
-  let content = "Create svelte kit web applications faster!"
+  let title = "SoundNeps"
+  let content = "It's a soundboard... with neps"
 </script>
 
 <head>
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="og:title" content={title}>
   <meta name="og:description" content={content}>
-  <meta name="og:image" content="">
+  <meta name="og:image" content="/page.jpeg">
   <meta name="og:type" content="website">
   <meta name="keywords" content="svelte">
   <title>{title}</title>
